aa3826be5bed78657888304a73324f6f4b35af73
L.E.D <89400654@ydu.edu.tw>
2015-05-20 15:30:49 +0800

    再次簽入專案

---
+++ TestFileMaker/TestFileMaker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestFileMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileTargetPath = @"E:\Test\";
+            for(int i=0;i<=10;i++)
+                Save(System.IO.Path.Combine(FileTargetPath,Guid.NewGuid().ToString()+".xml"));
+
+
+        }
+        static void Save(string FileFullName)
+        {
+            Console.WriteLine(FileFullName);
+            using (System.IO.StreamWriter Writer=new System.IO.StreamWriter(FileFullName))
+            {
+                for (int i = 0; i <= 10; i++)
+                {
+                    string Text = string.Format("{0}：第{1}行", DateTime.Now, i + 1);
+                    Console.WriteLine(Text);
+                    Writer.WriteLine(Text);
+                }
+            }
+        }
+    }
+}
